2dd9387ea0efaf966dd401be5765e49f960f533e
Swati Jha <swati@Saurabh-MacBook-Air.local>
2020-12-10 02:24:08 +0530

    ADD: mailer code

--- routes/testApi.js
+++ routes/testApi.js
@@ -1,12 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function (req, res, next) {
-  res.send('API is working properly');
-});
-
-module.exports = router;
-
 var nodemailer = require('nodemailer');
 const express = require('express');
 const bodyParser = require('body-parser');

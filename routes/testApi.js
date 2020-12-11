5b84fa59a244641094ccb0b3acd8fd11671b468a
Swati Jha <swati@Saurabh-MacBook-Air.local>
2020-12-11 18:39:49 +0530

    Connected Frontend and Backend

--- routes/testApi.js
+++ routes/testApi.js
@@ -1,81 +1,70 @@
-var nodemailer = require('nodemailer');
 const express = require('express');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const multer = require('multer');
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(bodyParser.json());
-var router = express.Router();
+const app = require('../app');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const api = express();
 
-var from;
-var email;
-var phone;
-var linkdin;
-var PortFolio;
-var path;
+//View Engine Setup
+api.engine('handlebars', exphbs());
+api.set('view-engines', 'handlebars');
 
-var Storage = multer.diskStorage({
-  /*/destination: function(req, file, callback) {
-        callback(null, "./pic");
-    },*/
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
-  },
+//Static Folder
+api.use('/public', express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.json());
+
+api.get('/', (req, res) => {
+  res.send('contact');
 });
 
-var upload = multer({
-  storage: Storage,
-}).single('file'); //Field name and max count
+api.post('/send', (req, res) => {
+  console.log(req.body);
 
-router.get('/', (req, res, next) => {
-  res.send('API is finally working');
-});
+  const email = req.body.email;
 
-router.post('/sendemail', (req, res) => {
-  upload(req, res, function (err) {
-    if (err) {
-      console.log(err);
-      return res.end('Something went wrong!');
+  const output = `
+  <p>You have a new Contact Request</p> 
+  <h3>Contact Details</h3>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone: ${req.body.phone}</li>
+  </ul>
+  `;
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'jhaswati0604@gmail.com',
+      pass: 'ranjanajha',
+    },
+
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  var mailOptions = {
+    from: 'jhaswati0604@gmail.com',
+    to: email,
+    subject: 'Hey',
+    html: output,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
     } else {
-      from = req.body.name;
-      email = req.body.email;
-      phone = req.body.phone;
-      linkdin = req.body.LinkedIn;
-      PortFolio = req.body.PortFolio;
-      path = req.file.path;
-
-      var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'rajdevanshueee@gmail.com',
-          pass: 'raJdevanshu1',
-        },
-      });
-
-      var mailOptions = {
-        from: email,
-        to: 'rajdevanshueee@gmail.com',
-        subject: `Resume from ${from}`,
-        html: `<li><ul>Name: ${from}</ul><ul>Phone: ${phone}</ul><ul>Linkdin: ${linkdin}</ul><ul>PortFolio: ${PortFolio}</ul></li><p>That was easy!</p>`,
-        attachments: [
-          {
-            path: path,
-          },
-        ],
-      };
-
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+      console.log('Email sent: ' + info.response);
     }
   });
 });
 
-module.exports = upload;
-module.exports = Storage;
-module.exports = router;
+console.log('Server Started...');
+
+module.exports = api;

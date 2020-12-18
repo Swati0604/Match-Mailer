a8a6951da7fa651f75e89eae6ef67f20dd24920c
Swati Jha <swati@Saurabh-MacBook-Air.local>
2020-12-18 13:01:44 +0530

    ADD: Attchment Code

--- routes/testApi.js
+++ routes/testApi.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const multer = require('multer');
 const app = require('../app');
-const path = require('path');
 const nodemailer = require('nodemailer');
 const api = express();
 
@@ -11,14 +10,16 @@ const api = express();
 api.engine('handlebars', exphbs());
 api.set('view-engines', 'handlebars');
 
-//Static Folder
-api.use('/public', express.static(path.join(__dirname, 'public')));
-
 //Body Parser Middleware
-api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
 
+var path;
+
 var Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, '../public/images');
+  },
   filename: function (req, file, callback) {
     callback(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
   },
@@ -26,7 +27,7 @@ var Storage = multer.diskStorage({
 
 var upload = multer({
   storage: Storage,
-}).single('file');
+}).single('resume');
 
 api.get('/', (req, res) => {
   res.send('contact');
@@ -36,33 +37,36 @@ api.post('/send', (req, res) => {
   console.log(req.body);
   upload(req, res, function (err) {
     const email = req.body.email;
-    // const path = req.file.path
-    // console.log(path);`r`
+
+    console.log(path);
+
+    console.log(req.body.file);
     const output = `
-  <p>Hi <span style={{font-weight:'bold'}}>${req.body.person},</span></p> 
-  <p>Hope you are doing great.</p>
-  <p>I came across this exciting opportunity available in your organiation. I am intrested in applying for the position of <span style={{font-weight:'bold'}}>UI/UX Designer at Myntra</span></span>. I feel my skiils and experience will be a valuable asset to your firm.
-  <br><br>I have attached my Resume & Portfolio link for your consideration. Please take a moment to go through them to get a better picture of who I am.
-  <br><br></p>
-  
-  <p><span style={{font-weight:'bold'}}>PortFolio:</span> ${req.body.portfolio}</p>
-  <p><span style={{font-weight:'bold'}}>LinkedIn:</span> ${req.body.linkedin}</p>
+      <p>Hi <span style={{font-weight:'bold'}}>${req.body.person},</span></p> 
+      <p>Hope you are doing great.</p>
+      <p>I came across this exciting opportunity available in your organiation. I am intrested in applying for the position of <span style={{font-weight:'bold'}}>UI/UX Designer at Myntra</span></span>. I feel my skiils and experience will be a valuable asset to your firm.
+      <br><br>I have attached my Resume & Portfolio link for your consideration. Please take a moment to go through them to get a better picture of who I am.
+      <br><br></p>
+      
+      <p><span style={{font-weight:'bold'}}>PortFolio:</span> ${req.body.portfolio}</p>
+      <p><span style={{font-weight:'bold'}}>LinkedIn:</span> ${req.body.linkedin}</p>
 
-  <p>I would love to talk to you regarding this amazing opportunity in your organization.</p>
+      <p>I would love to talk to you regarding this amazing opportunity in your organization.</p>
 
-  <br>
-  <p>Thanks in advance,</p>
-    <p>${req.body.name}</p>
-    <p>${req.body.email}</p>
-    <p>${req.body.phone}</p>
- 
-  `;
+      <br>
+      <p>Thanks in advance,</p>
+        <p>${req.body.name}</p>
+        <p>${req.body.email}</p>
+        <p>${req.body.phone}</p>
+         <p>${req.body.resume}</p>
+    
+      `;
 
     var transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: 'jhaswati0604@gmail.com',
-        pass: 'mumma@06',
+        user: 'refferal.designsundays@gmail.com',
+        pass: 'refferal@DS123',
       },
 
       tls: {
@@ -75,11 +79,12 @@ api.post('/send', (req, res) => {
       to: req.body.contact,
       subject: 'Hey',
       html: output,
-      /*  attachments: [
-       {
-       path: path
-       }
-    ]*/
+      attachments: [
+        {
+          path: path,
+          contentType: 'application/pdf',
+        },
+      ],
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
